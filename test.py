59d9d0f298e7a16fc9df0a72811f31640a1e4796
MaKxex <maksimos651@gmail.com>
2023-04-12 23:42:40 +0300

    minor fix

--- test.py
+++
@@ -1,9 +0,0 @@
-def get_all_divisors_brute(n):
-    for i in range(1, int(n / 2) + 1):
-        if n % i == 0 and i > 1:
-            yield i
-    yield n
-
-
-for x in get_all_divisors_brute(61):
-    print(x)
